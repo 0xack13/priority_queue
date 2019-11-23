9fe907eb61b152ad39e7f51a6ca25a5e6618c75e
Jakub Tesárek <mail@jakubtesarek.cz>
2019-11-23 19:53:35 +0100

    Refactor queue to use priority heap instead of list

--- priority_queue/queue.py
+++ priority_queue/queue.py
@@ -1,34 +1,54 @@
+import itertools
+import heapq
+
 class EmptyError(Exception):
     pass
 
 
 class Node:
     # TODO: Use dataclass in python 3.7
-    def __init__(self, value, priority):
+    def __init__(self, value, priority, order):
         self.value = value
         self.priority = priority
+        self.order = order
+        self.is_deleted = False
+
+    def delete(self):
+        self.is_deleted = True
+        self.value = None
+
+    def __bool__(self):
+        return not self.is_deleted
+
+    def __lt__(self, other):
+        if self.priority == other.priority:
+            return self.order < other.order
+        return self.priority > other.priority
 
 
 class PriorityQueue:
     def __init__(self):
-        self.data = [] # TODO: Optimize by using heap
+        self.heap = [] # Items organized in heap
+        self.entry_finder = {} # Map of items to find if item already is in queue
+        self.counter = itertools.count() # Items counter to keep sorting stable
 
     def put(self, value, priority):
-        node = self._find_value_in_data(value)
-        if node:
-            node.priority += priority
-        else:
-            node = Node(value, priority)
-            self.data.append(node)
-        self.data.sort(key=lambda n: n.priority, reverse=True)
+        if value in self.entry_finder:
+            orig_node = self._pop_node_by_value(value)
+            priority += orig_node.priority
+        node = Node(value, priority, next(self.counter))
+        self.entry_finder[value] = node
+        heapq.heappush(self.heap, node)
+
+    def _pop_node_by_value(self, value):
+        node = self.entry_finder.pop(value)
+        node.delete()
+        return node
 
     def get(self):
-        try:
-            return self.data.pop(0).value
-        except IndexError:
-            raise EmptyError()
-
-    def _find_value_in_data(self, value):
-        for n in self.data:
-            if n.value == value:
-                return n
+        while self.heap:
+            node = heapq.heappop(self.heap)
+            if node:
+                del self.entry_finder[node.value]
+                return node.value
+        raise EmptyError()
---
+++ tests/node.py
@@ -0,0 +1,30 @@
+import pytest
+
+from priority_queue.queue import Node
+
+
+def test_create_node():
+    node = Node('A', 2, 3) 
+    assert node.value == 'A'
+    assert node.priority == 2
+    assert node.order == 3
+    assert bool(node)
+
+
+def test_delete_node():
+    node = Node('A', 2, 3)
+    node.delete()
+    assert node.value is None
+    assert not bool(node)
+
+
+def test_sort_nodes_by_priority():
+    node1 = Node('A', 0, 1)
+    node2 = Node('B', 1, 2)
+    assert node1 > node2
+
+
+def test_stable_sort_of_nodes():
+    node1 = Node('A', 1, 1)
+    node2 = Node('B', 1, 2)
+    assert node1 < node2

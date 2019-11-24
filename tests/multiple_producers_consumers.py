8c1ec6970a7437d2048af3059594a9a2cb3d788c
Jakub Tesárek <mail@jakubtesarek.cz>
2019-11-24 15:07:15 +0100

    test multiple producers, one consumer

--- tests/multiple_producers_consumers.py
+++ tests/multiple_producers_consumers.py
@@ -22,6 +22,16 @@ def thread_get(results, queue, block=True, timeout=0.3):
     return threading.Thread(target=_get, args=(results, queue, block, timeout))
 
 
+def thread_get_n(results, queue, n,  block=True, timeout=0.1):
+    def _get_n(results, queue, n, block, timeout):
+        try:
+            r = queue.get_n(n, block, timeout)
+        except Exception as e:
+            r = [e]
+        results.extend(r)
+    return threading.Thread(target=_get_n, args=(results, queue, n,  block, timeout))
+
+
 def test_many_producers_and_consumers():
     q = PriorityQueue()
     r = []
@@ -34,3 +44,17 @@ def test_many_producers_and_consumers():
     for t in threads:
         t.join()
     assert sorted(r) == sorted(string.ascii_letters) # Has to be sorted, put() is called in random order
+
+
+def test_many_producers_and_one_getn_consumer():
+    q = PriorityQueue()
+    r = []
+    threads = []
+    for priority, letter in enumerate(string.ascii_letters):
+        threads.append(thread_put(r, q, letter, priority))
+    threads.append(thread_get_n(r, q, len(string.ascii_letters)))
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+    assert sorted(r) == sorted(string.ascii_letters)
